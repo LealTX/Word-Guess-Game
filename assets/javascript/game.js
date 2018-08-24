98e0602be5abb1771802a6a159e8c17868576074
LealTX <jacob.leal32@gmail.com>
2018-08-23 22:56:33 -0500

    Added guess blanks and keypress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,18 +5,30 @@ var lettersCorrect = [];
 var guessRemaining = 8;
 var wins = 0;
 var losses = 0;
+var answerArray = [];
 
 //Add the text stuff here
 var directionsText = document.getElementById("directionTextID");
+var winsText = document.getElementById("winsTextID");
+var currentWordText = document.getElementById("currentWordTextID");
+var guessRemainingText = document.getElementById("guessRemainingTextID");
+var letterGuessedText = document.getElementById("letterGuessedTextID");
 
 
 document.getElementById("directionTextID").addEventListener("click", function() {
     var computerGuess = batWords[Math.floor(Math.random() * batWords.length)];
     console.log("Computer Selects: "+computerGuess)
-    
+
+    for (var i = 0; i < computerGuess.length; i++) {
+        answerArray[i] = " _ ";
+    }
+
     directionsText.textContent = "";
 
-
+    winsText.textContent = "Wins: "+ wins;
+    currentWordText.textContent = "Current Word:   " + answerArray;
+    guessRemainingText.textContent = "Lives Left: "+ guessRemaining;
+    letterGuessedText = "Letters Guessed:" + lettersGuessed;
 }, true);
 
 //Function
@@ -24,12 +36,7 @@ document.onkeydown = function(event) {
     var userGuess = event.key;
 
     //Hide Direction
-    console.log("Computer Selects: "+computerGuess)
     console.log("userGuess: "+userGuess);
-    console.log(batWords.indexOf(computerGuess));
-    batWords.splice(batWords.indexOf(computerGuess),1);
-    console.log(batWords);
-
 
 }
 
