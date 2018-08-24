b5a10949133e91e9f76c3e71da6c226d395e51b0
LealTX <jacob.leal32@gmail.com>
2018-08-24 15:54:38 -0500

    Added the function to replace blanks with letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,12 @@
 var batWords = ["bruce", "wayne", "pennyworth", "scarecrow", "bane"];
-
+ //Define vars
 var lettersGuessed = [];
 var lettersCorrect = [];
 var guessRemaining = 8;
 var wins = 0;
 var losses = 0;
 var answerArray = [];
+var computerGuess;
 
 //Add the text stuff here
 var directionsText = document.getElementById("directionTextID");
@@ -14,7 +15,7 @@ var currentWordText = document.getElementById("currentWordTextID");
 var guessRemainingText = document.getElementById("guessRemainingTextID");
 var letterGuessedText = document.getElementById("letterGuessedTextID");
 
-
+//Start game on click; This also selects computerGuess
 document.getElementById("directionTextID").addEventListener("click", function() {
     var computerGuess = batWords[Math.floor(Math.random() * batWords.length)];
     console.log("Computer Selects: "+computerGuess)
@@ -29,14 +30,21 @@ document.getElementById("directionTextID").addEventListener("click", function() 
     currentWordText.textContent = "Current Word:   " + answerArray;
     guessRemainingText.textContent = "Lives Left: "+ guessRemaining;
     letterGuessedText = "Letters Guessed:" + lettersGuessed;
-}, true);
 
-//Function
+//Users starts to guess
 document.onkeydown = function(event) {
     var userGuess = event.key;
-
-    //Hide Direction
     console.log("userGuess: "+userGuess);
+    console.log("Computer Guess:" + computerGuess);
 
+    if (userGuess.length > 0){
+        for (var i = 0; i < computerGuess.length; i++){
+            if (computerGuess[i] === userGuess) {
+                answerArray[i] = userGuess;
+            }
+        }
+        guessRemaining--;
+        document.getElementById("currentWordTextID").innerHTML = answerArray.join(" ");
+    }
 }
-
+});
