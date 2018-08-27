9eaf699f13ec700b944973667bbed2a483567a0b
LealTX <jacob.leal32@gmail.com>
2018-08-27 09:18:54 -0500

    functioning working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,26 @@
 var batWords = ["bruce", "wayne", "pennyworth", "scarecrow", "bane"];
- //Define vars
+//Define vars
 var lettersGuessed = [];
 var lettersCorrect = [];
-var guessRemaining = 8;
+var guessRemaining = 5;
 var wins = 0;
 var losses = 0;
 var answerArray = [];
 var computerGuess;
+var letterIncorrect = true;
 
 //Add the text stuff here
 var directionsText = document.getElementById("directionTextID");
 var winsText = document.getElementById("winsTextID");
+var hiddenWord = document.getElementById("hiddenWordID");
 var currentWordText = document.getElementById("currentWordTextID");
 var guessRemainingText = document.getElementById("guessRemainingTextID");
 var letterGuessedText = document.getElementById("letterGuessedTextID");
 
 //Start game on click; This also selects computerGuess
-document.getElementById("directionTextID").addEventListener("click", function() {
-    var computerGuess = batWords[Math.floor(Math.random() * batWords.length)];
-    console.log("Computer Selects: "+computerGuess)
+document.getElementById("directionTextID").addEventListener("click", function () {
+    computerGuess = batWords[Math.floor(Math.random() * batWords.length)];
+    guessRemaining = 5;
 
     for (var i = 0; i < computerGuess.length; i++) {
         answerArray[i] = " _ ";
@@ -26,25 +28,65 @@ document.getElementById("directionTextID").addEventListener("click", function() 
 
     directionsText.textContent = "";
 
-    winsText.textContent = "Wins: "+ wins;
-    currentWordText.textContent = "Current Word:   " + answerArray;
-    guessRemainingText.textContent = "Lives Left: "+ guessRemaining;
-    letterGuessedText = "Letters Guessed:" + lettersGuessed;
+    hiddenWord.textContent = "Word";
+    currentWordText.textContent = answerArray;
+    winsText.textContent = "Wins: " + wins;
+    guessRemainingText.textContent = "Lives Left: " + guessRemaining;
+    letterGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
 
-//Users starts to guess
-document.onkeydown = function(event) {
-    var userGuess = event.key;
-    console.log("userGuess: "+userGuess);
-    console.log("Computer Guess:" + computerGuess);
-
-    if (userGuess.length > 0){
-        for (var i = 0; i < computerGuess.length; i++){
-            if (computerGuess[i] === userGuess) {
-                answerArray[i] = userGuess;
-            }
-        }
-        guessRemaining--;
-        document.getElementById("currentWordTextID").innerHTML = answerArray.join(" ");
-    }
-}
 });
+//Users starts to guess
+document.onkeydown = function (event) {
+    var userGuess = event.key;
+    console.log("userGuess: " + userGuess);
+    console.log("Computer Guess:" + computerGuess);
+    lettersGuessed.push(userGuess);
+    letterGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
+
+
+    for (var i = 0; i < computerGuess.length; i++) {
+        if (computerGuess[i] === userGuess) {
+            answerArray[i] = userGuess;
+            document.getElementById("currentWordTextID").innerHTML = answerArray.join(" ");
+            letterIncorrect = false;
+        }
+        console.log(letterIncorrect);
+
+    }
+
+    if (letterIncorrect) {
+        guessRemaining--;
+        guessRemainingText.textContent = "Lives Left: " + guessRemaining;
+    }
+
+    if (guessRemaining == 0) {
+        console.log("You lose!!");
+        playAgain();
+    }
+
+    letterIncorrect = true;
+
+    if (!answerArray.includes(" _ ")) {
+        wins++;
+        winsText.textContent = "Wins: " + wins;
+        console.log("You Win!!");
+        playAgain();
+        
+    }
+
+}
+
+function playAgain () {
+    computerGuess = batWords[Math.floor(Math.random() * batWords.length)];
+    guessRemaining = 5;
+    answerArray = [];
+    for (var i = 0; i < computerGuess.length; i++) {
+        answerArray[i] = " _ ";
+    }
+    lettersGuessed = [];
+    currentWordText.textContent = answerArray;
+    guessRemainingText.textContent = "Lives Left: " + guessRemaining;
+    letterGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
+}
+
+
