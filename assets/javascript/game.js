f7d307dac3db270a07326c4014192c3579407ff3
LealTX <jacob.leal32@gmail.com>
2018-08-27 17:07:40 -0500

    Style the site a bit more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,7 @@ var letterIncorrect = true;
 //Add the text stuff here
 var directionsText = document.getElementById("directionTextID");
 var winsText = document.getElementById("winsTextID");
+var lossesText = document.getElementById("lossesTextID");
 var hiddenWord = document.getElementById("hiddenWordID");
 var currentWordText = document.getElementById("currentWordTextID");
 var guessRemainingText = document.getElementById("guessRemainingTextID");
@@ -31,6 +32,7 @@ document.getElementById("directionTextID").addEventListener("click", function ()
     hiddenWord.textContent = "Word";
     currentWordText.textContent = answerArray;
     winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
     guessRemainingText.textContent = "Lives Left: " + guessRemaining;
     letterGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
 
@@ -43,7 +45,8 @@ document.onkeydown = function (event) {
     lettersGuessed.push(userGuess);
     letterGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
 
-
+//Computer Checks if the letter is in the word,
+//if correct set letterIncorrect to false
     for (var i = 0; i < computerGuess.length; i++) {
         if (computerGuess[i] === userGuess) {
             answerArray[i] = userGuess;
@@ -53,19 +56,25 @@ document.onkeydown = function (event) {
         console.log(letterIncorrect);
 
     }
-
+//If incorrect then it removes life
     if (letterIncorrect) {
         guessRemaining--;
         guessRemainingText.textContent = "Lives Left: " + guessRemaining;
     }
 
+//Reset letterIncorrect
+    letterIncorrect = true;
+
+//Checks lives, if 0 You Lose! and reset game
     if (guessRemaining == 0) {
+        alert("You lose!!");
+        losses++;
+        lossesText.textContent = "Losses: " + losses;
         console.log("You lose!!");
         playAgain();
     }
 
-    letterIncorrect = true;
-
+//If no more _ then You Win! and Reset game
     if (!answerArray.includes(" _ ")) {
         wins++;
         winsText.textContent = "Wins: " + wins;
@@ -76,6 +85,7 @@ document.onkeydown = function (event) {
 
 }
 
+//Function to Reset the game
 function playAgain () {
     computerGuess = batWords[Math.floor(Math.random() * batWords.length)];
     guessRemaining = 5;
